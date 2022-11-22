62e50e24e8204fce3bbc3b92355a5b92c1daca8a
Elliot Baynes <sbaynes1994@gmail.com>
2022-11-21 22:05:36 -0600

    add basic form submission handler
    waiting on other services WIP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-console.log(import.meta.env.VITE_googleAPI);
-import.meta.env.VITE_yelpAPI;
+// import yelpApiService from "./src/yelp.api.service";
+// import mapsApiService from "./src/maps.api.service";
+// import renderService from "./src/render";
+
+// import.meta.env.VITE_googleAPI;
+// import.meta.env.VITE_yelpAPI;
+
+document
+  .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const citySearched = event.target.city.value;
+    console.log(citySearched);
+
+    const saveKey = event.target.city.value;
+    const saveValue = event.target.city.value;
+
+    localStorage.setItem(saveKey, saveValue);
+    console.log(localStorage);
+    // const yelpData = await yelpApiService.getLocation(citySearched);
+
+    // const mapData = await mapsApiService.getLocation(citySearched);
+    // renderService.renderList(yelpData)
+
+    // renderService.renderMap(mapData)
+  });
+// citySearched -> into yelpApiService
+// citySearched -> into mapsApiService
+// run yelpApi -> into renderService
+// run mapsApi -> into renderService
+//   local storage: save the location searched using the input as the key and value
+// document.ready: iterate over localstorage keys. generate button for each key with a text value = saveValue
+// on button click, run function = form.submit using text value as the citySearched
