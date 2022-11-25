1cbf2466c39467ea66be3c2c29b225e20ac754c0
Elliot Baynes <sbaynes1994@gmail.com>
2022-11-25 17:59:24 -0600

    add correct function to generate list nested lists

--- src/render.js
+++ src/render.js
@@ -1,10 +1,27 @@
 // todo: function to create Li and nested UL
-const resultsList = document.querySelector("#results ul");
+// varaible from element with id of "results"
+const resultsList = document.querySelector("#results");
 
-function renderResults(results) {
-  for (let i = 0; results.length; i++) {
-    resultsList.appendChild("li");
-  }
-}
+export default {
+  // function to render results from yelpAPI to page
+  renderResults(yelpData) {
+    // iterate over returned results from yelp and make nested unordered list
+    for (let i = 0; i < yelpData.businesses.length; i++) {
+      // create an hmtl fragment
+      const listFragment = document.createDocumentFragment();
 
-export default {};
+      // define html fragment
+      const nestedList = listFragment
+        .appendChild(document.createElement("li"))
+        .appendChild(document.createElement("ul"));
+
+      // add CSS classes for tailwind to created UL
+      nestedList.setAttribute(
+        "class",
+        "mt-1 grid grid-cols-3 rounded bg-amber-500 p-2 text-center"
+      );
+      // append the generated fragment to the document
+      resultsList.appendChild(listFragment);
+    }
+  },
+};
