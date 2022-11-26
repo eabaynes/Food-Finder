e2e3834d38f4cbc1e793a15c839cf9e8769abe1d
EllieWalsch <elliewalsch4@gmail.com>
2022-11-26 16:44:13 -0600

    Handle history button click

--- src/index.js
+++ src/index.js
@@ -14,13 +14,12 @@ document
 
     const coordinates = yelpData.region.center;
 
-    localStorage.setItem(citySearched, JSON.stringify(coordinates));
+    localStorage.setItem("cityCoords", JSON.stringify(coordinates));
     localStorage.setItem("history", citySearched);
 
     // display restaurant info in list
     renderService.renderResults(yelpData);
     renderService.renderYelp(yelpData);
-    // renderService.renderHistory();
 
     function initMap() {
       const map = new google.maps.Map(document.getElementById("map"), {
@@ -52,3 +51,42 @@ function initPage() {
 }
 
 initPage();
+
+const historyButton = document.querySelector("#history button");
+
+historyButton.addEventListener("click", async function () {
+  const citySearched = localStorage.getItem("history");
+
+  // get list of restaurants based on searched city
+  const yelpData = await yelpApiService.getRestaurants(citySearched);
+  console.log(yelpData);
+
+  // display restaurant info in list
+  renderService.renderResults(yelpData);
+  renderService.renderYelp(yelpData);
+
+  // render new map from saved coordinates
+  function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: yelpData.region.center.latitude,
+        lng: yelpData.region.center.longitude,
+      },
+      zoom: 12,
+    });
+
+    // make a marker for each restaurant
+    for (let i = 0; i < yelpData.businesses.length; i++) {
+      const marker = new google.maps.Marker({
+        position: {
+          lat: yelpData.businesses[i].coordinates.latitude,
+          lng: yelpData.businesses[i].coordinates.longitude,
+        },
+        map,
+        title: yelpData.businesses[i].name,
+      });
+    }
+  }
+
+  initMap();
+});
