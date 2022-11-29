b68eb3eaa770584c2053f081e91b1a2cc7f3dab4
Elliot Baynes <sbaynes1994@gmail.com>
2022-11-28 20:49:02 -0600

    move yelpAPI

--- src/yelp.api.service.js
+++ src/yelp.api.service.js
@@ -1,6 +1,6 @@
-const yelpAPI = import.meta.env.VITE_yelpAPI;
 const baseURL = `https://cors-anywhere-bg.herokuapp.com/`;
-
+const yelpAPI =
+  "FY9FXRq5nGT1Y02G12V4h85GFeCoXRIkGbnOqZxOfEOcF89_Me_kxNYd0iZD8cXixIAk6-zwR7OzLbZqhuC0hXipI4GF8aymEjeeoJDWg0vvFX3DCoSrnuE8ueJ2Y3Yx";
 // Get user entered city and get restaurants that offer delivery nearby
 // NOTE: Yelp API doesn't have full functionality with less populated cities yet
 export default {
